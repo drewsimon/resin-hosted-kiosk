8ed25a0c28eb25bc195ace32a6fb1053c9f52fbd
Drew Simon <drew@ordermygear.com>
2015-03-16 12:04:29 -0500

    debugging

--- startx.sh
+++ startx.sh
@@ -6,8 +6,7 @@ ldconfig
 
 echo `whoami`
 echo $RESIN_DEVICE_UUID
-URL=${$RES$IN_DEVICE_UUID:-"NO URL FOUND"}
-#URL=${$RESIN_DEVICE_UUID:-"https://teamomg.geckoboard.com/loop/F74A13D1272AECD9"}
-echo $URL;
+DEFAULT_URL="NO URL FOUND"
+echo $(echo $RESIN_DEVICE_UUID)
 
 /usr/bin/startx
