dbbfac0d882a6765bfcd7b3ef70ca9abad88dac7
Drew Simon <drew@ordermygear.com>
2015-03-16 13:30:29 -0500

    default url

--- startx.sh
+++ startx.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
-echo 'HERE WE GO!'
-
 ldconfig
 
 #printenv
-echo `whoami`
 URL_VAR=URL_${RESIN_DEVICE_UUID}
-echo $URL_VAR
 eval URL=\$$URL_VAR
 echo $URL
 echo $DEFAULT_URL
-URL=${URL-`echo $DEFAULT_URL`}
+#URL=${URL-`echo $DEFAULT_URL`}
 echo $URL
 
 #/usr/bin/startx
