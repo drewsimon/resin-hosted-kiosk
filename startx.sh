82f15910b49fea4b6368e3bd093ed649c8919208
Drew Simon <drew@ordermygear.com>
2015-03-16 12:19:34 -0500

    debugging

--- startx.sh
+++ startx.sh
@@ -7,8 +7,8 @@ ldconfig
 echo `whoami`
 echo $RESIN_DEVICE_UUID
 URL_VAR="RESIN_$RESIN_DEVICE_UUID"
-echo "VAR:" + URL_VAR
+echo "VAR:" + $URL_VAR
 DEFAULT_URL="NO URL FOUND"
-echo "URL:" + $(echo $RESIN_DEVICE_UUID)
+echo "URL:" + $(echo $URL_VAR)
 
 /usr/bin/startx
