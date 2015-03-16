8fe1e0bc00974501e5cf3122ca44d8c7035c5cbc
Drew Simon <drew@ordermygear.com>
2015-03-16 14:36:09 -0500

    replace url with env variables

--- startx.sh
+++ startx.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ldconfig
 
-#printenv
 URL_VAR=URL_${RESIN_DEVICE_UUID}
 eval URL=\$$URL_VAR
 echo $URL
 echo $DEFAULT_URL
-#URL=${URL-`echo $DEFAULT_URL`}
+URL=${URL:=$DEFAULT_URL}
 echo $URL
 
-#/usr/bin/startx
+sed -i "s|APP_URL|$URL|g" /root/xinitrc
+
+/usr/bin/startx
