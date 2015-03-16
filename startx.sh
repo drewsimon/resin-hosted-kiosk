3c2b103dc048c36435d69aef2ff74b4e8b1b5b99
Drew Simon <drew@ordermygear.com>
2015-03-16 11:57:06 -0500

    read url from env variable

--- startx.sh
+++ startx.sh
@@ -5,5 +5,9 @@ echo 'HERE WE GO!'
 ldconfig
 
 echo `whoami`
+echo $RESIN_DEVICE_UUID
+URL=${$RES$IN_DEVICE_UUID:-"NO URL FOUND"}
+#URL=${$RESIN_DEVICE_UUID:-"https://teamomg.geckoboard.com/loop/F74A13D1272AECD9"}
+echo $URL;
 
 /usr/bin/startx
