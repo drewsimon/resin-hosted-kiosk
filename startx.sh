5e6c117458341260b5b8161ad15eb8a0faf1abfd
Drew Simon <drew@ordermygear.com>
2015-03-16 13:20:19 -0500

    debug

--- startx.sh
+++ startx.sh
@@ -4,10 +4,11 @@ echo 'HERE WE GO!'
 
 ldconfig
 
-printenv
+#printenv
 echo `whoami`
 DEFAULT_URL="NO URL FOUND"
 URL_VAR=URL_${RESIN_DEVICE_UUID}
+echo $URL_VAR
 eval URL=\$$URL_VAR
 echo $URL
 URL=${URL-`echo $DEFAULT_URL`}
