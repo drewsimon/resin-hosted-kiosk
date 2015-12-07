9804cfddee52fc64bacef782678f0d02f5e247b5
Drew Simon <drew@ordermygear.com>
2015-12-07 17:27:25 -0600

    debug info

--- startx.sh
+++ startx.sh
@@ -3,13 +3,15 @@ ldconfig
 
 URL_VAR=URL_${RESIN_DEVICE_UUID}
 eval URL=\$$URL_VAR
-echo $URL
-echo $DEFAULT_URL
 URL=${URL:=$DEFAULT_URL}
-echo $URL
+
+echo "Setting url to: $URL"
 
 sed -i "s|APP_URL|$URL|g" /root/.xinitrc
 
 touch /root/.Xauthority
+
+cat /etc/hostname
+cat /etc/hosts
 
 /usr/bin/startx
