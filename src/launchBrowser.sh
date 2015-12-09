bd276a8377ba9080594ccde67ff184518c9276c1
Drew Simon <drew@ordermygear.com>
2015-12-09 10:01:41 -0600

    data dir

--- src/launchBrowser.sh
+++ src/launchBrowser.sh
@@ -5,10 +5,10 @@
  xset s off
  xset s noblank
 
-mkdir /root/.config
+mkdir -p /root/.config
 url=$URL
 default='https://www.ordermygear.com'
 sudo matchbox-window-manager -use_cursor no -use_titlebar no &
 xte 'sleep 15' 'key F11'&
-chromium-browser  --app=${url:-$default} --no-sandbox
+chromium-browser  --app=${url:-$default} --no-sandbox --user-data-dir=/root/.config
 sleep 2s
